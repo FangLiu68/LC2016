796afd31acd66a3fa52a0b766051ff871024a5fc
Fang Liu <FL7272@yp.com>
2016-03-21 17:22:43 -0700

    Initial Commit

---
+++ LC2016/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LC2016
+//
+//  Created by Fang Liu on 3/21/16.
+//  Copyright © 2016 Fang Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
