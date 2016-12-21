2642d9d6d91c18ce427fbabad012c2e2b4296230
Fang Liu <FL7272@yp.com>
2016-12-21 11:43:29 -0800

    max depth of binary tree

--- LC2016/Max Depth of Binary Tree.cpp
+++ LC2016/Max Depth of Binary Tree.cpp
@@ -12,6 +12,19 @@
  The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
  */
 
+/*
+ Note:
+ the depth of a tree is log(N)
+        1           1 = 2^0
+      /   \
+     2     3        2 = 2^1
+    / \   /  \
+   4   5 6    7     4 = 2^2
+   ....             ...
+ total nodes num n = 2^0+2^1+2^2+...+2^h = 2^(h+1)-1
+ h = log(N)
+ */
+
 #include "BinaryTree.h"
 #include <iostream>
 using namespace std;
